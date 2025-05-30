dc15527eedf48dda3a41fb84124a68b569934fad
DANY-505 <fernandezobito30@gmail.com>
2025-05-30 11:13:00 -0500

    version1
    primera migracion

---
+++ view/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page failed</title>
+
+</head>
+<body>
+    <div class="error">
+        <p>ERROR</p>
+        <h1>404</h1>
+        <p>PAGE NOT FOUND</p>
+    </div>
+
+     <style>
+        body{
+            background-color:black;
+            
+        }
+        .error{
+            color:white;
+            text-align:center;
+            font-size: 30px;
+        }
+    </style>
+</body>
+</html>
---
+++ view/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-box">
+        <form action="inicio.php" method="POST" autocomplete="off">
+            <h2>LOGIN</h2>
+            <label class="label">Usuario</label>
+            <div class="input-box">
+                <input type="text" required>
+        
+            </div>
+            
+            <label class="label">Contraseña</label>           
+            <div class="input-box"> 
+                <input type="password" required>
+            </div>
+
+            <button type="submit">Iniciar Sesion</button>
+
+        </form>
+    </div>
+
+
+</body>
+</html>
+
